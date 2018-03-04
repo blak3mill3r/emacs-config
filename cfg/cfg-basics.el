43ff5c7fc6b4ec1eb0c90a91c93ad9f9964212d3
Blake Miller <blake@magnathorax.iris.tv>
2018-03-04 20:01:18 +0000

    I'm learning that general syntax is quite nice

--- cfg/cfg-basics.el
+++ cfg/cfg-basics.el
@@ -109,30 +109,29 @@
 (setq evil-want-integration nil)
 (use-package evil
   :general
-  (general-define-key
-   :keymaps 'normal
-   "SPC" 'avy-goto-char-timer
-   :prefix ","
-   :keymaps 'normal
-   "b."     'next-buffer
-   "b,"     'previous-buffer
-   "bd"     'kill-buffer
-   "bl"     'buffer-menu
-   "b SPC"  'ido-switch-buffer
-   "wd"     'delete-window
-   "wb"     'delete-other-windows
-   "bk"     'kill-buffer-and-window
-   "bd"     'kill-buffer
-   "br"     'revert-buffer
-   ",s"     'shell
-   "pa"     'projectile-ag
-   ",/"     'ido-find-file
-   ",."     'ido-dired
-   ",x"     'smex
-   ",b"     'ivy-switch-buffer
-   ",,x"    'smex-major-mode-commands
-   ",w"     'make-frame
-   "gs"     'magit-status)
+  (:keymaps 'normal
+            "SPC" 'avy-goto-char-timer
+            :prefix ","
+            :keymaps 'normal
+            "b."     'next-buffer
+            "b,"     'previous-buffer
+            "bd"     'kill-buffer
+            "bl"     'buffer-menu
+            "b SPC"  'ido-switch-buffer
+            "wd"     'delete-window
+            "wb"     'delete-other-windows
+            "bk"     'kill-buffer-and-window
+            "bd"     'kill-buffer
+            "br"     'revert-buffer
+            ",s"     'shell
+            "pa"     'projectile-ag
+            ",/"     'ido-find-file
+            ",."     'ido-dired
+            ",x"     'smex
+            ",b"     'ivy-switch-buffer
+            ",,x"    'smex-major-mode-commands
+            ",w"     'make-frame
+            "gs"     'magit-status)
   :demand t
   :config
   (progn
