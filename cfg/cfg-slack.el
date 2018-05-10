6020afa990d350f28c9a1b963224e4e44d5b85dd
blak3mill3r <blak3mill3r@gmail.com>
2018-05-10 02:49:55 +0000

    add slack and alert packages

---
+++ cfg/cfg-slack.el
@@ -0,0 +1,54 @@
+(use-package alert
+  :commands (alert)
+  :init
+  (setq alert-default-style 'libnotify))
+
+(use-package slack
+  :commands (slack-start)
+  :init
+  (setq slack-buffer-emojify t) ;; if you want to enable emoji, default nil
+  (setq slack-prefer-current-team t)
+  :config
+  (slack-register-team
+   :name "IRIS"
+   :default t
+   :client-id "aaaaaaaaaaa.00000000000"
+   :client-secret "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
+
+   ;; https://github.com/yuya373/emacs-slack#1-from-a-query-param
+   :token iris-slack-token
+
+   :subscribed-channels '(devteam)
+   :full-and-display-names t
+   )
+
+  ;; (slack-register-team
+  ;;  :name "test"
+  ;;  :client-id "3333333333.77777777777"
+  ;;  :client-secret "cccccccccccccccccccccccccccccccc"
+  ;;  :token "xxxx-yyyyyyyyyy-zzzzzzzzzzz-hhhhhhhhhhh-llllllllll"
+  ;;  :subscribed-channels '(hoge fuga))
+
+  (evil-define-key 'normal slack-info-mode-map
+    ",u" 'slack-room-update-messages)
+  (evil-define-key 'normal slack-mode-map
+    ",c" 'slack-buffer-kill
+    ",ra" 'slack-message-add-reaction
+    ",rr" 'slack-message-remove-reaction
+    ",rs" 'slack-message-show-reaction-users
+    ",pl" 'slack-room-pins-list
+    ",pa" 'slack-message-pins-add
+    ",pr" 'slack-message-pins-remove
+    ",mm" 'slack-message-write-another-buffer
+    ",me" 'slack-message-edit
+    ",md" 'slack-message-delete
+    ",u" 'slack-room-update-messages
+    ",2" 'slack-message-embed-mention
+    ",3" 'slack-message-embed-channel
+    "\C-n" 'slack-buffer-goto-next-message
+    "\C-p" 'slack-buffer-goto-prev-message)
+  (evil-define-key 'normal slack-edit-message-mode-map
+    ",k" 'slack-message-cancel-edit
+    ",s" 'slack-message-send-from-buffer
+    ",2" 'slack-message-embed-mention
+    ",3" 'slack-message-embed-channel))
