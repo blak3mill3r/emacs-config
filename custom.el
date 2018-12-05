18e3005818f44fe08f5f2643134082c624baae04
Blake Miller <blak3mill3r@gmail.com>
2018-12-05 17:57:21 +0000

    customize default face

--- custom.el
+++ custom.el
@@ -14,4 +14,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "PfEd" :family "Inconsolata")))))
