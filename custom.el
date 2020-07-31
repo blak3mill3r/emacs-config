0c493ccb6ab723b36795cd916bd5cba24cecdfb6
blak3mill3r <blak3mill3r@gmail.com>
2020-07-31 01:15:41 +0000

    custom settings (slightly smaller default face)

--- custom.el
+++ custom.el
@@ -3,16 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(browse-url-browser-function (quote browse-url-chrome))
+ '(browse-url-browser-function 'browse-url-chrome)
  '(inhibit-startup-screen t)
  '(safe-local-variable-values
-   (quote
-    ((haskell-process-use-ghci . t)
-     (intero-targets "xmonad-blak3mill3r:exe:xmonad")
-     (intero-stack-yaml . "/home/blake/w/wsconfig/my-xmonad/stack.yaml")))))
+   '((intero-stack-yaml . "/home/blake/w/nerdvana/xmonadsrc/stack.yaml")
+     (haskell-process-use-ghci . t)
+     (intero-targets "xmonad-blak3mill3r:exe:xmonad"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 280 :width normal :foundry "PfEd" :family "Inconsolata")))))
+ '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 240 :width normal :foundry "PfEd" :family "Inconsolata")))))
